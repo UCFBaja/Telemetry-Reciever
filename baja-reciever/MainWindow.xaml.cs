3b99e887fe6dab2114d5e66ecce0def9b4f2b6b4
Chris House <chris@housetech.guru>
2023-03-31 11:19:34 -0400

    Fixed Format bugs and incorrect string format crash

--- baja-reciever/MainWindow.xaml.cs
+++ baja-reciever/MainWindow.xaml.cs
@@ -146,21 +146,21 @@ namespace baja_reciever
         {
             for (int i = 0; i < data.Length; i++)
             {
-                // Make sure there is actually data in the element
-                if (data[i].Length > 0)
+                // Make sure there is actually data in the element, and the data is longer than the sensor identifier
+                if (data[i].Length > 0 && data[i].Length > sensor.Length)
                 {
                     // Looking for the first 3-4 characters
                     string search = data[i].Substring(0, sensor.Length);
 
                     if (search.Equals(sensor))
                     {
-                        /* Console Debug Statements
-                        Console.WriteLine(data[i]);
+                        /*Console Debug Statements
+                        Console.WriteLine("\"" + data[i] + "\"");
                         Console.WriteLine(data[i].Length);
                         return data[i].Substring(3, data[i].Length-1);
-                        Console.WriteLine(data[i].Substring(sensor.Length - 1, data[i].Length - 6));
+                        Console.WriteLine(data[i].Substring(sensor.Length + 1, data[i].Length - 3));
                         */
-                        return data[i].Substring(sensor.Length + 1, data[i].Length - 6);
+                        return data[i].Substring(sensor.Length + 1, data[i].Length - 3);
                     }
 
                 }
